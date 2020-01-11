fa43e996eb295272b9e4c07b9f1b8a81b847850c
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-11 11:05:54 -0700

    implemented redux with song list component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,14 @@
 import React from "react";
+import SongList from "./SongList";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="ui container grid" style={{ marginTop: "1rem" }}>
+      <div className="ui row">
+        <div className="eight wide column">
+          <SongList />
+        </div>
+      </div>
+    </div>
+  );
 }
