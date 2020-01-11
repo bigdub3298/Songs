43756c6a009a825a1c42c09ab2e52289a57634e5
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-10 19:51:58 -0700

    implemented reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Pyramids", duration: "7:40" },
+    { title: "Life is Good", duration: "3:58" },
+    { title: "Big Stepper", duration: "2:55" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
